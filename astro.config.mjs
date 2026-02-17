3c836ab6f10d15cd531a5c675e09e3fcc957ee48
Bill Zuo <bill@billsmac.local>
2026-02-16 19:50:47 -0800

    Fix Cloudflare deploy to static Workers/Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://softprobe.ai', // Update this with your actual domain
-  output: 'server',
-  adapter: cloudflare(),
+  output: 'static',
   integrations: [tailwind()],
   build: {
     assets: 'assets'
