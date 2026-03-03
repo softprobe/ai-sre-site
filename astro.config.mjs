5a1b1b7618bbbcde288dc74a21b92e62b50f4ec7
husain-meetava <husain@meetava.com>
2026-03-02 21:26:56 -0800

    Fix social share preview and favicon issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://softprobe.ai', // Update this with your actual domain
+  site: 'https://softprobe.dev', // Update this with your actual domain
   output: 'static',
   integrations: [tailwind()],
   build: {
